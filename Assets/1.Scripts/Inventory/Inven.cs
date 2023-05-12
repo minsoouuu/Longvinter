4d43ecce82bd4aec48e18c8b2d08795645b7bd2d
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-05-12 21:50:06 +0900

    5.12
    5.12

--- Assets/1.Scripts/Inventory/Inven.cs
+++ Assets/1.Scripts/Inventory/Inven.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Inven : MonoBehaviour
 {
+    [SerializeField] private GameObject inven_tile;
+    [SerializeField] private Transform parent;
+ 
     // Start is called before the first frame update
     void Start()
     {
-        int b = 0;
+        CreateInven();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void CreateInven()
+    {
+        Instantiate(inven_tile, parent);
     }
 }
