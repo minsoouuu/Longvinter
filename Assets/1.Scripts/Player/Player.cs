59a634515ed669003a44d67343df7a03c445518a
minsoouuu <118816600+minsoouuu@users.noreply.github.com>
2023-05-12 19:42:15 +0900

    123123

--- Assets/1.Scripts/Player/Player.cs
+++ Assets/1.Scripts/Player/Player.cs
@@ -17,16 +17,32 @@ public abstract class Player : MonoBehaviour
     [SerializeField] private State state = State.None;
     [SerializeField] private Animator animator;
 
+    float maxHP = 100f;
     float speed = 3f;
+    float curHP = 0f;
+    float damage = 10f;
 
-    void Start()
+    public float HP
     {
-        
+        get { return curHP; }
+        set
+        { 
+            curHP = value;
+            // 죽을때 상태 처리
+        }
     }
     public float Speed
     {
         get { return speed; }
         set { speed = value; }
+    }
+    public float Damage
+    {
+        get { return damage; }
+    }
+    void Start()
+    {
+        curHP = maxHP;
     }
     void FixedUpdate()
     {
@@ -35,7 +51,6 @@ public abstract class Player : MonoBehaviour
         Move();
         Direction();
     }
-
     void Move()
     {
         float x = Input.GetAxisRaw("Horizontal");
