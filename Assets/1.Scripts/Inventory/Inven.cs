8779c33b876c2e06aa7b552428a7275fbe2a3420
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-05-15 17:24:52 +0900

    123
    123

--- Assets/1.Scripts/Inventory/Inven.cs
+++ Assets/1.Scripts/Inventory/Inven.cs
@@ -6,10 +6,19 @@ public class Inven : MonoBehaviour
 {
     [SerializeField] private GameObject inven_tile;
     [SerializeField] private Transform parent;
- 
+
+    [HideInInspector] public int InvenX { get; set; }
+    [HideInInspector] public int InvenY { get; set; }
+
+    List<GameObject> invenlist = new List<GameObject>();
+
     // Start is called before the first frame update
     void Start()
     {
+        // 인벤토리창 가로 4개, 세로로 8줄 총 32개
+        InvenX = 4;
+        InvenY = 8;
+
         CreateInven();
     }
 
@@ -19,8 +28,18 @@ public class Inven : MonoBehaviour
         
     }
 
+    // 인벤토리 뒤에 타일 생성
     public void CreateInven()
     {
-        Instantiate(inven_tile, parent);
+        for(int i = 0; i < InvenX * InvenY; i++)
+        {
+            GameObject gb =  Instantiate(inven_tile, parent);
+            invenlist.Add(gb);
+        }
+    }
+    
+    public void AddItem()
+    {
+
     }
 }
