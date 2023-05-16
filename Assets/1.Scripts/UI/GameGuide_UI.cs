02ad4c225e283f2610a9f3d1c66f6979409b1914
dhf0527 <rbals0527@naver.com>
2023-05-16 20:11:19 +0900

    123
    123

---
+++ Assets/1.Scripts/UI/GameGuide_UI.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameGuide_UI : MonoBehaviour
+{
+    [SerializeField] private ToggleGroup tgroup;
+    [SerializeField] private List<Toggle> tg_list = new List<Toggle>();
+    [SerializeField] private List<GameObject> pg_list = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
