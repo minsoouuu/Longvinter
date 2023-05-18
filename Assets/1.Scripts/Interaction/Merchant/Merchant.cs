51ad53bdb88b3fed3787b1caaa8a0c3a09630453
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-05-18 21:54:18 +0900

    상인 UI, 기능 구현
    상인 UI, 기능 구현

---
+++ Assets/1.Scripts/Interaction/Merchant/Merchant.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Merchant : MonoBehaviour
+{
+    [SerializeField] private GameObject b_itemlist;
+    [SerializeField] private Transform b_parent;
+    [SerializeField] private GameObject s_itemlist;
+    [SerializeField] private Transform s_parent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateMerchant_b_ItemList();
+        CreateMerchant_s_ItemList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateMerchant_b_ItemList()
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            Instantiate(b_itemlist, b_parent);
+        }
+    }
+    void CreateMerchant_s_ItemList()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Instantiate(s_itemlist, s_parent);
+        }
+    }
+
+    public void Onclick_Buy()
+    {
+
+    }
+
+    public void Onclick_Sell()
+    {
+
+    }
+    
+}
