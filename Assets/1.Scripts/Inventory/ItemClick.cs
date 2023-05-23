3a510c3b18aa4b160dd509d66f1abbeb5ed79dc1
Soyeon Shin <soyeon@swu.ac.kr>
2023-05-23 17:42:48 +0900

    ItemClick_1

---
+++ Assets/1.Scripts/Inventory/ItemClick.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClick : MonoBehaviour
+{
+    Inventory inventory = new Inventory();
+    Slot slot = new Slot();
+    [SerializeField] private Transform target = null;
+    [SerializeField] private GameObject buttons;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        LeftClick();
+    }
+
+    public void LeftClick()
+    {
+        //??? ???? ??? ? ???? ?? ???
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Input.mousePosition;
+            pos.z = Camera.main.farClipPlane;
+
+            target.position = Camera.main.ScreenToWorldPoint(pos);
+            Debug.Log(target);
+
+            Instantiate(buttons, target);
+        }
+    }
+
+    public void ItemChuck()
+    {
+        slot.DeleteItem();
+        Destroy(buttons);
+    }
+
+    public void ItemUse()
+    {
+        Debug.Log("???? ???????!");
+        Destroy(buttons);
+    }
+  
+}
