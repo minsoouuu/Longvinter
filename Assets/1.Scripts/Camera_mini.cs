4f3fc3a4aa4e50dbc06ab048c2844bbebaf5e397
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-06-15 18:37:18 +0900

    minimap prefab

--- Assets/1.Scripts/Camera_mini.cs
+++ Assets/1.Scripts/Camera_mini.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class Camera_mini : MonoBehaviour
 {
-    
+    Vector3 pos;
+
+    float pos_x = 0f;
+    float pos_y = 2.5f;
+    float pos_z = 0f;
     private void Start()
     {
         
     }
     void Update()
     {
-        transform.position = Gamemanager.instance.player.transform.position;
+        pos = new Vector3(Gamemanager.instance.player.transform.position.x + pos_x, Gamemanager.instance.player.transform.position.y + pos_y, Gamemanager.instance.player.transform.position.z + pos_z);
+        transform.position = pos;
     }
 }
