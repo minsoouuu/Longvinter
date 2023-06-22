a8e2056a9c56c6b521c5b9bb6d8729f8beffa976
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-06-22 17:49:57 +0900

    Game Guide 수정
    Game Guide 수정

--- Assets/1.Scripts/UI/GameGuide_UI.cs
+++ Assets/1.Scripts/UI/GameGuide_UI.cs
@@ -2,22 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UI.Extensions;
 
 public class GameGuide_UI : MonoBehaviour
 {
-    [SerializeField] private ToggleGroup tgroup;
-    [SerializeField] private List<Toggle> tg_list = new List<Toggle>();
-    [SerializeField] private List<GameObject> pg_list = new List<GameObject>();
-
+    [SerializeField] private HorizontalScrollSnap hs;
+    [SerializeField] private Toggle[] toggles;
+    Toggle curToggle;
+    int curPageNum;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeToggle(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        curPageNum = hs._currentPage;
+        ChangeToggle(curPageNum);
+    }
+
+    void ChangeToggle(int a)
+    {
+        curToggle = toggles[a];
+        curToggle.isOn = true;  
+        curToggle.Select();
     }
 }
