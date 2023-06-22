51a527d460b76852dacd46ca281a2145032e125c
Soyeon Shin <thdus060978@naver.com>
2023-06-22 18:35:46 +0900

    Lobby_AudioSetting

---
+++ Assets/1.Scripts/AudioManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public Slider BgmSlider;
+    public Slider SfxSlider;
+    public GameObject mute;
+
+    public void Sfx()
+    {
+        AudioSource sfxButton1 = GetComponent<AudioSource>();
+        sfxButton1.Play();
+    }
+
+    public void BGMAudioControl()
+    {
+        audioMixer.SetFloat("BGM", Mathf.Log10(BgmSlider.value) * 20);
+
+    }
+
+    public void SFXAudioControl()
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(SfxSlider.value) * 20);
+
+    }
+
+    public void ToggleAudioVolume()
+    {
+        AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+    }
+}
