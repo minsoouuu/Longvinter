f588479dce7fc6de33773e671997a4a5b15a40f9
Joo-Sung-Hwan <96138046+Joo-Sung-Hwan@users.noreply.github.com>
2023-06-26 17:52:57 +0900

    house 변경 중

--- Assets/1.Scripts/House/House.cs
+++ Assets/1.Scripts/House/House.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class House : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject house_UI;
+    [SerializeField] private 
+    float dis;
 
+    private void Start()
+    {
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        dis = Vector3.Distance(this.transform.position, Gamemanager.instance.player.transform.position);
+        Debug.Log(dis);
+        if(dis < 2f)
+        {
+            house_UI.SetActive(true);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                SceneManager.LoadScene("House");
+            }
+        }
     }
 }
